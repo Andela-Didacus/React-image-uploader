7937179005aac22fc7fa876172f22324fe838ba6
Andela-Didacus <didacus.abongo@andela.com>
2018-01-12 15:33:04 +0300

    feat(images-uploader): create images component

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import ReactDom from 'react-dom';
+import Images from './components/images';
 
 class App extends Component {
     render() {
         return (
             <div>
                 React app
+                <Images/>
             </div>
         )
     }
